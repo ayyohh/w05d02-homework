9ef02d80c79d0b6d8998e4a2dba85ce230b0598c
Alex <fuckyobitch@Alexs-MacBook-Pro.local>
2018-07-12 15:04:38 -0600

    Added all restful routes, full functionality

--- pokemon_app/models/pokemon.js
+++ pokemon_app/models/pokemon.js
@@ -1,10 +1,50 @@
-const pokemon = [ {name: "Bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"},
-				{name: "Ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur.jpg"},
-				{name: "Venusaur", img: "http://img.pokemondb.net/artwork/venusaur.jpg"},
-				{name: "Charmander", img: "http://img.pokemondb.net/artwork/charmander.jpg"},
-				{name: "Charizard", img: "http://img.pokemondb.net/artwork/charizard.jpg"},
-				{name: "Squirtle", img: "http://img.pokemondb.net/artwork/squirtle.jpg"},
-				{name: "Wartortle", img: "http://img.pokemondb.net/artwork/wartortle.jpg"}
-			  ];
 
-module.exports = pokemon;
+
+const pokemon = [
+	{id: 1, name: "Bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"},
+	{id: 2, name: "Ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur.jpg"},
+	{id: 3, name: "Venusaur", img: "http://img.pokemondb.net/artwork/venusaur.jpg"},
+	{id: 4, name: "Charmander", img: "http://img.pokemondb.net/artwork/charmander.jpg"},
+	{id: 6, name: "Charizard", img: "http://img.pokemondb.net/artwork/charizard.jpg"},
+	{id: 7, name: "Squirtle", img: "http://img.pokemondb.net/artwork/squirtle.jpg"},
+	{id: 8, name: "Wartortle", img: "http://img.pokemondb.net/artwork/wartortle.jpg"}
+];
+
+class Pokemon {
+	constructor(data){
+		this.name = data['name'];
+		this.image = data['image'];
+		this.id = data['id']
+	}
+	static create(data){
+		let newPoke = new Pokemon(data);
+		pokemon.push(newPoke);
+		console.log(pokemon);
+	}
+	static all(){
+		return pokemon;
+	}
+	static findOne(id){
+		for (let i = 0; i < pokemon.length; i++){
+			if(id == pokemon[i]['id']){
+				return pokemon[i]
+			}
+		}
+	}
+	static update(id, updatedPoke){
+		let ogPoke = Pokemon.findOne(id);
+		ogPoke['name'] = updatedPoke['name'];
+		ogPoke['image'] = updatedPoke['image'];
+		console.log(pokemon);
+	}
+	static delete(id) {
+		for (let i = 0; i < pokemon.length; i++) {
+			if (pokemon[i].id === id) {
+				pokemon.splice(i, 1)
+			}
+		}
+	}
+}
+
+console.log(Pokemon.findOne(1));
+module.exports = Pokemon
--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,26 +1,59 @@
 const express = require('express');
 const app = express();
-const poke = require('./models/pokemon.js');
+const Poke = require('./models/pokemon.js');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
+
+//Get get
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon home page');
 });
 
 app.get('/pokemon', (req, res) => {
   res.render('index.ejs', {
-    pokes: poke
+    pokes: Poke.all()
   });
+});
+
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs', {})
 });
 
 app.get('/pokemon/:id', (req, res) => {
-  console.log(poke[req.params.id]);
   res.render('show.ejs', {
-
-    pokes: poke[req.params.id]
+    pokes: Poke.findOne(req.params.id)
   });
 });
 
+//Edit
+app.get('/pokemon/:id/edit', (req, res) => {
+  res.render('edit.ejs', {
+    pokes: Poke.findOne(req.params.id)
+  });
+});
+
+//Create
+app.post('/pokemon', (req, res) => {
+  Poke.create(req.body);
+  res.redirect('/pokemon');
+});
+
+//Update
+app.post('/pokemon/:id', (req, res) => {
+  Poke.update(req.params.id, req.body);
+  	res.redirect("/pokemon/" + req.params.id);
+});
+
+//DESTROY
+app.delete('/pokemon/:id', (req, res) => {
+  Poke.delete(req.params.id);
+  res.redirect('/pokemon');
+});
 
 
 
